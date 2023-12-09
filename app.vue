507b0076a9b65e2e30e88e3a588638a88e8cdee4
Robert Long <robertvessnalong@gmail.com>
2023-12-09 13:51:19 -0500

    Test iPhone island

--- app.vue
+++ app.vue
@@ -4,6 +4,12 @@
 
 <script lang="ts" setup>
 useHead({
+  meta: [
+    {
+      name: "apple-mobile-web-app-status-bar-style",
+      content: "bg-gradient-to-r from-[#108dc7] to-[#ef8e38]",
+    },
+  ],
   titleTemplate: (titleChunk) => {
     return titleChunk
       ? `${titleChunk} - Site Title`
